fe49a100b72faf6e7f1d563a38369376778818e8
Burhan Ahmed <burhan.ahmed@advantal.net>
2025-08-24 11:16:56 +0530

    summary added.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from "./components/Layout/Header";
 import "./App.css"
+import MealsSummary from "./components/Meals/MealSummary";
 function App() {
   return (
     <div>
       <Header />
-      
+      <MealsSummary/>
     </div>
   );
 }
